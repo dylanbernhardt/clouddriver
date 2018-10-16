933518c9ff0b59287953f7ed45e6ad4d0151718d
Dylan Bernhardt <dylan.bernhardt@target.com>
2018-10-16 12:01:02 -0500

    ost image patch

--- clouddriver-openstack/src/test/groovy/com/netflix/spinnaker/clouddriver/openstack/client/OpenstackImageV1ClientProviderSpec.groovy
+++ clouddriver-openstack/src/test/groovy/com/netflix/spinnaker/clouddriver/openstack/client/OpenstackImageV1ClientProviderSpec.groovy
@@ -18,15 +18,9 @@ package com.netflix.spinnaker.clouddriver.openstack.client
 
 import com.netflix.spinnaker.clouddriver.openstack.deploy.exception.OpenstackProviderException
 import org.openstack4j.api.exceptions.ServerResponseException
-<<<<<<< HEAD:clouddriver-openstack/src/test/groovy/com/netflix/spinnaker/clouddriver/openstack/client/OpenstackImageV1ClientProviderSpec.groovy
-import org.openstack4j.api.image.ImageService
-import org.openstack4j.model.image.Image
-import org.openstack4j.model.network.Subnet
-=======
 import org.openstack4j.api.image.v2.ImageService
 import org.openstack4j.model.image.v2.Image
 import org.openstack4j.openstack.image.v2.domain.GlanceImage
->>>>>>> b5c9524e0... fix(provider/openstack): Vendor upstream opstack4j fixes.:clouddriver-openstack/src/test/groovy/com/netflix/spinnaker/clouddriver/openstack/client/OpenstackImageV2ClientProviderSpec.groovy
 import org.springframework.http.HttpStatus
 
 class OpenstackImageV2ClientProviderSpec extends OpenstackClientProviderSpec {
@@ -35,14 +29,10 @@ class OpenstackImageV2ClientProviderSpec extends OpenstackClientProviderSpec {
     setup:
     Map<String, String> filters = null
     ImageService imageService = Mock(ImageService)
-<<<<<<< HEAD:clouddriver-openstack/src/test/groovy/com/netflix/spinnaker/clouddriver/openstack/client/OpenstackImageV1ClientProviderSpec.groovy
-    List<Image> images = [Mock(Image)]
-=======
     GlanceImage.Location imageLocation = new GlanceImage.Location(url: "http://example.com/image.iso")
     Image image = Mock(Image) {
       getLocations() >> [imageLocation]
     }
->>>>>>> b5c9524e0... fix(provider/openstack): Vendor upstream opstack4j fixes.:clouddriver-openstack/src/test/groovy/com/netflix/spinnaker/clouddriver/openstack/client/OpenstackImageV2ClientProviderSpec.groovy
 
     when:
     List<Subnet> result = provider.listImages(region, filters)
@@ -52,12 +42,8 @@ class OpenstackImageV2ClientProviderSpec extends OpenstackClientProviderSpec {
     1 * imageService.listAll(filters) >> images
 
     and:
-<<<<<<< HEAD:clouddriver-openstack/src/test/groovy/com/netflix/spinnaker/clouddriver/openstack/client/OpenstackImageV1ClientProviderSpec.groovy
-    result == images
-=======
     result[0] instanceof OpenstackImage
     result[0].location == imageLocation.url
->>>>>>> b5c9524e0... fix(provider/openstack): Vendor upstream opstack4j fixes.:clouddriver-openstack/src/test/groovy/com/netflix/spinnaker/clouddriver/openstack/client/OpenstackImageV2ClientProviderSpec.groovy
     noExceptionThrown()
   }
 
